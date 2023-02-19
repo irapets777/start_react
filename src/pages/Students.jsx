b2f6a0f3e5de07adf6facde307c4b2295e8c3301
Iryna Pec <118050604+irapets777@users.noreply.github.com>
2023-02-19 14:07:08 +0200

    Update Students.jsx

--- src/pages/Students.jsx
+++ src/pages/Students.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
 const Students = () => {
+  // create a state variable to store  the post
+  let [posts, setPosts] = useState([]);
+  //create a fnction to fetch the post from the API
+  async function getPosts() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then ((res) => res.json())
+    .then ((data) => {
+      setPosts(data);
+    });
+}
   return (
     <div>Students</div>
+
   )
 }
 
